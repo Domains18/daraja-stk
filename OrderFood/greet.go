d3bbd399d77126b787dcdfcab979039ff50b61e2
Domains18 <gibsonsgibson88@gmail.com>
2024-01-04 09:40:29 +0300

    golang code work for OrderFood dir

---
+++ OrderFood/greet.go
@@ -0,0 +1,17 @@
+package OrderFood
+
+import "fmt"
+
+func greet(customerName string) {
+	fmt.Printf("%52s %s\n", "Namaste ", customerName)
+	fmt.Printf("%72s\n", "_/\\_ Welcome to Our Shop _/\\_ ")
+	fmt.Println()
+}
+
+func bye(customerName string) {
+	fmt.Println()
+	fmt.Printf("%17s", " ")
+	fmt.Printf("_/\\_ Thank you %v for visiting Us! _/\\_\n", customerName)
+	fmt.Printf("%55s\n", "We hope to see you again!")
+	fmt.Printf("%51s\n", "Have a nice day! ")
+}
--- OrderFood/menu.go
+++ OrderFood/menu.go
@@ -1,28 +1,41 @@
 package OrderFood
 
-
-
+import (
+	"fmt"
+	"strings"
+)
 
 type Menu struct {
-	itemNo uint
-	itemName string
+	itemNo    uint
+	itemName  string
 	itemPrice float64
 }
 
 var menu = []Menu{
 	//itemNo  itemName   itemPrice
-	{1, "Adrakh Chai", 20},
+	{1, "Chai", 20},
 	{2, "Filter Coffee", 25},
-	{3, "Chhas", 35.50},
-	{4, "Lassi", 30},
-	{5, "Mango Lassi", 60},
-	{6, "Kadi Chaawal", 50},
-	{7, "Chhole Bhature", 45},
-	{8, "Khasta Kachori", 30},
-	{9, "Raj Kachori", 30},
+	{3, "black coffee", 35.50},
+	{4, "Latte", 30},
+	{5, "Mango Juice", 60},
+	{6, "red tea", 50},
+	{7, "Yellow tea", 45},
+	{8, "Black Tea", 30},
+	{9, "Green tea", 30},
 	{10, "Veg. Sandwich", 20},
-	{11, "Veg. Masala Maggi", 60.00},
+	{11, "Tea Masala", 60.00},
 	{12, "Samosa", 20},
 	{13, "Cream Roll", 15},
 }
 
+func printMenu() {
+	fmt.Printf("%15s\n", "Menu")
+	fmt.Printf("%s\n", strings.Repeat("-", 35))
+	fmt.Printf("%-7s %6s    %12s\n", "S.No.", "Price", "Item Name")
+	fmt.Printf("%s\n", strings.Repeat("-", 35))
+	for _, element := range menu {
+		fmt.Printf(" %-7d %.2f    %-4s\n", element.itemNo, element.itemPrice, element.itemName)
+	}
+	fmt.Printf("%s", strings.Repeat("-", 35))
+	fmt.Println()
+}
