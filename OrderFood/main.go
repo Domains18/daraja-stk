8e94467e508427d8a73d1142b742fa7dd1bc42f4
Domains18 <gibsonsgibson88@gmail.com>
2024-01-05 23:07:48 +0300

    golang application

--- OrderFood/main.go
+++ OrderFood/main.go
@@ -1,7 +1,21 @@
 package OrderFood
 
+import "fmt"
 
+var subTotalBill float64
 
-
-var subTotalBill float64;
 var customerOrder = make(map[string]uint, 0)
+
+func main() {
+	var customerName string
+	fmt.Println("what is your firstName")
+	fmt.Scan(&customerName)
+
+	greet(customerName)
+	orderItems()
+	displayGeneratingBill()
+	generateBill()
+	modifyOrder()
+	printFinalBill()
+	bye(customerName)
+}
